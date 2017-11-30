157e3696dde71fb8a3afa6a27564101e57b85c67
NelllW <nelllw@WL-203-176.WPA.Claremont.Edu>
2017-11-30 13:52:23 -0500

    Start training on all previous notes, rather than smaller sections

--- ThirdTry.py
+++ ThirdTry.py
@@ -7,6 +7,10 @@ from keras.layers.recurrent import LSTM
 
 
 TMP_MAX_LEN= 16*4
+LONGEST = 16*16
+
+CCREST = [1]*8 + [0]*12
+CC_SIZE = 20
 
 
 
@@ -116,7 +120,10 @@ def unwrap(arr):
 
     flatArr = []
     for a in arr:
-        flatArr += a
+        if type(a) == list:
+            flatArr += a
+        else:
+            flatArr.append(a)
 
     return flatArr
 
@@ -216,6 +223,96 @@ def modelCC(pieces, numPieces, numLines, N_values, N_epochs):
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     model.fit(sentences, next_values, batch_size=1, epochs=N_epochs)
+
+    return model
+
+
+def modelCCPadding(pieces, numPieces, numLines, N_values, N_epochs):
+
+    #numLines = 4
+    #N_values = 13
+    max_len = TMP_MAX_LEN
+    #N_epochs = 1000
+
+    # cut the corpus into semi-redundant sequences of max_len values
+    step = 16
+    sentences = []
+    #x = np.array([])
+    next_values = []
+
+    samples = 0 # this will get calculated as we make things
+
+    for piece in pieces:
+        print "len pice", len(piece) - 1
+        for i in range(0, min(len(piece) - 1, LONGEST), step):
+            print "i", i
+            #print "piece i", piece[i]
+
+            # unwrap all note vectors
+            past = []
+            for note in piece[:i]:
+                past += unwrap(note)
+
+            # pad the beginning of past until it is of length LONGEST
+            restTime = []
+            for line in range(numLines):
+                restTime.append(CCREST)
+            past = unwrap(restTime)*(LONGEST-i) + past
+            #print "past", past
+            print "len past", len(past), LONGEST*CC_SIZE*numLines
+
+            #print "past", past
+            #print "past", unwrap(past)
+
+            #sentences.append(past)
+            sentences += past
+
+
+
+            #print "flat", unwrap(piece[i + max_len + 1])
+            #next_values.append(unwrap(piece[i]))
+            next_values += unwrap(piece[i])
+            print "NV", next_values
+
+            samples += 1
+        #print('nb sequences:', len(sentences))
+
+
+    #X = np.zeros((len(sentences), LONGEST, numLines*N_values), dtype=np.bool)
+    #y = np.zeros((len(sentences), numLines*N_values), dtype=np.bool)
+
+    #sentence = np.reshape(sentences, (numPieces, ))
+    #print "sentences", sentences
+
+    uSentences = unwrap(sentences)
+    #print "uSentences", uSentences
+    #print "end uSentences"
+    print "samples", samples, "len answers", len(next_values)
+
+    x = np.array(uSentences)
+    x = np.reshape(x, (samples, LONGEST, CC_SIZE*numLines))
+    print "x", x
+
+    y = np.array(next_values)
+    y = np.reshape(y, (samples, CC_SIZE*numLines))
+
+    #print "\n\n\nx", X
+    #print "\n\n\ny", y
+
+    # make LSTM
+    model = Sequential()
+    model.add(LSTM(400, return_sequences=False, input_shape=(LONGEST, numLines*N_values)))
+    model.add(Dropout(0.2))
+    #model.add(LSTM(200, return_sequences=True, input_shape=(max_len, numLines*N_values)))
+    #model.add(Dropout(0.1))
+    #model.add(LSTM(200, return_sequences=False))
+    #model.add(Dropout(0.1))
+    model.add(Dense(numLines*N_values))
+    model.add(Activation('hard_sigmoid')) # used to be softmax. consider
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    model.fit(x, y, batch_size=1, epochs=N_epochs)
 
     return model
 
@@ -396,18 +493,81 @@ def trainOn4(N_epochs):
     thsize = 20 # num ints in twoHotHorizontal
     numLines = 3
     numPieces = 4
-    m = modelCC([p1cc], numPieces, numLines, thsize, N_epochs)
+    m = modelCCPadding([p1cc], numPieces, numLines, thsize, N_epochs)
 
-    v = '1'
+    v = '4'
 
-    predictStuff('outAOF' + v + '.csv', predCC, numLines, thsize, m)
-    predictStuff('outWTC3' + v + '.csv', p1cc, numLines, thsize, m)
-    predictStuff('outWTC9' + v + '.csv', p2cc, numLines, thsize, m)
-    predictStuff('outSAOF' + v + '.csv', p3cc, numLines, thsize, m)
-    predictStuff('outFF' + v + '.csv', p4cc, numLines, thsize, m)
+    predictStuffPadding('outAOF' + v + '.csv', predCC, numLines, thsize, m)
+    predictStuffPadding('outWTC3' + v + '.csv', p1cc, numLines, thsize, m)
+    predictStuffPadding('outWTC9' + v + '.csv', p2cc, numLines, thsize, m)
+    predictStuffPadding('outSAOF' + v + '.csv', p3cc, numLines, thsize, m)
+    predictStuffPadding('outFF' + v + '.csv', p4cc, numLines, thsize, m)
 
 
 
+
+def predictStuffPadding(outfile, predCC, numLines, thsize, m):
+
+
+    first = predCC[0: 0 + TMP_MAX_LEN]
+    first = np.reshape(first, (1,TMP_MAX_LEN,numLines*thsize))
+
+    currentPred = first # the current four measures we're predicting off of
+
+    # pad currentPred
+    restTime = []
+    for line in range(numLines):
+        restTime.append(CCREST)
+    #rtnp = np.array(unwrap(restTime*(LONGEST - len(currentPred))))
+    #rtnp = np.reshape(rtnp, (1, LONGEST-len(currentPred), CC_SIZE*numLines))
+    print "restTime", restTime
+    #print "currentPred", currentPred
+    flatCurrentPred = unwrap(np.ndarray.tolist(currentPred[0]))
+    print "flat curr pred len", len(flatCurrentPred)
+    flatRestTime = unwrap(unwrap(restTime)*(LONGEST-TMP_MAX_LEN))
+    print "flat rest len", len(flatRestTime)
+    currentPred = np.array(flatRestTime + flatCurrentPred)
+    print "currentPred padded", currentPred
+    currentPred = np.reshape(currentPred, (1,LONGEST, CC_SIZE*numLines))
+
+    #currentPred = np.concatenate((rtnp, currentPred), axis = 1)
+
+    print currentPred
+    fullPred = first # the full prediction
+
+    # predict a string of 32 notes
+    lenComp = 16*4
+    for i in range(lenComp):
+        pred = m.predict(currentPred)
+        pred = np.reshape(pred, (1,numLines*thsize))
+
+        # put each note in a general oneHotHorizontal arrangement
+        CCPred = []
+        #for noteVec in pred:
+        # go through each note vector in the prediction 
+        listPred = np.ndarray.tolist(pred)
+        for line in range(0,numLines*thsize,thsize):
+
+            CCPred.append(goodRepCC(listPred[0][line:line+thsize]))
+
+        newData = np.reshape(np.asarray(CCPred), (1,numLines*thsize))
+
+        currentPred = np.concatenate((currentPred[0][1:], newData), axis=0)
+        currentPred = np.reshape(currentPred, (1,LONGEST, numLines*CC_SIZE))
+
+        fullPred = np.concatenate((fullPred[0], newData), axis=0)
+        fullPred = np.reshape(fullPred, (1,TMP_MAX_LEN+i+1, numLines*thsize))
+  
+
+    # how we used to reshape things:
+    fullPred = np.reshape(fullPred, (TMP_MAX_LEN+lenComp, numLines, thsize))
+    fullPredArray = np.ndarray.tolist(fullPred)
+    #print "fullPredArray", fullPredArray
+
+    # transform oneHotHorizontal to piece and then to csv
+    predPiece = fromCC(fullPredArray)
+    predPieceCsv = open(outfile, 'w')
+    predPieceCsv.write(predPiece.csv())
 
 
 
@@ -545,7 +705,7 @@ def trainFullFugue(N_epochs):
 def main(args):
     #a = [0.8633742928504944, 0.8520313501358032, 0.6155416965484619, 0.8277215957641602, 0.7745899558067322, 0.6654524803161621, 0.5017514824867249, 0.5754479169845581, 0.3196893334388733, 0.364773690700531, 0.2010064721107483, 0.3020211458206177, 0.21267035603523254, 0.22699597477912903, 0.23443511128425598, 0.2467953860759735, 0.32214605808258057, 0.1710146963596344, 0.18880566954612732, 0.21740081906318665]
     #print goodRepCC(a)
-    trainOn4(500)
+    trainOn4(250)
 
 
 
