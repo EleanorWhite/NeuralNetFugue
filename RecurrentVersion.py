58aac49f0fad9e0e7e58ecc8fcf9b14083d77fac
NelllW <nelllw@WL-206-219.WPA.Claremont.Edu>
2017-12-01 23:50:45 -0500

    functional recurrent version

--- RecurrentVersion.py
+++ RecurrentVersion.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import SimpleRNN
 
 
 TMP_MAX_LEN= 16*4
@@ -80,8 +81,8 @@ def firstBelowInd(arr, filter):
     return len(arr)
 
 def goodRepCC(vec):
-    ''' This takes a predicted note vector of the TwoHotHorizontal form
-    and converts it to correct TwoHotHorizontal form'''
+    ''' This takes a predicted note vector of the CC form
+    and converts it to correct CC form'''
 
     zFilt = .3 # anything below this is a 0 and above is a 1
 
@@ -238,6 +239,7 @@ def modelCCPadding(pieces, numPieces, numLines, N_values, N_epochs):
     #sentence = np.reshape(sentences, (numPieces, ))
     #print "sentences", sentences
 
+
     uSentences = unwrap(sentences)
     #print "uSentences", uSentences
     #print "end uSentences"
@@ -271,61 +273,157 @@ def modelCCPadding(pieces, numPieces, numLines, N_values, N_epochs):
     return model
 
 
-def getPieceSections(filename, firstLen, secondLen, thirdLen):
+def modelRecurrent(x,y, numPieces, numLines, N_epochs):
+
+    max_len = TMP_MAX_LEN
+    #N_epochs = 1000
+
+
+    # make LSTM
+    model = Sequential()
+    model.add(SimpleRNN(100, return_sequences=False, input_shape=(1, CC_SIZE*numLines*MAX_SECT_LEN)))
+    model.add(Dropout(0.2))
+    #model.add(LSTM(200, return_sequences=True, input_shape=(max_len, numLines*N_values)))
+    #model.add(Dropout(0.1))
+    #model.add(LSTM(200, return_sequences=False))
+    #model.add(Dropout(0.1))
+    model.add(Dense(numLines*CC_SIZE*MAX_SECT_LEN))
+    model.add(Activation('hard_sigmoid')) # used to be softmax. consider
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    model.fit(x, y, batch_size=1, epochs=N_epochs)
+
+    return model
+
+
+def getPieceSections(filename, firstLen, secondLen, thirdLen, numLines):
     p = Piece()
     p.fromCSV(filename)
     pcc = p.getCC()
-    first = pcc[0:firstLen]
-    second = pcc[firstLen: firstLen+secondLen]
-    third = pcc[firstLen+secondLen: thirdLen]
+
+    restTime = []
+    for line in range(numLines):
+        restTime.append(CCREST)
+
+    first = pcc[0:firstLen] + [restTime]*(MAX_SECT_LEN-firstLen)
+    #print "first w/o rests", pcc[0:firstLen]
+    #print "First", first
+    second = pcc[firstLen: firstLen+secondLen] + [restTime]*(MAX_SECT_LEN-secondLen)
+    #print "second", second
+    third = pcc[firstLen+secondLen: firstLen+secondLen +thirdLen]+ [restTime]*(MAX_SECT_LEN-thirdLen)
+
+    print "firstlen1", len(first)
+    print "firstlen2", len(second)
+    print "firstlen3", len(third)
 
     # create x and y so that x = section n corresponds to y = section n+1
-    x = np.array([unwrap(first), unwrap(second)])
-    x = np.reshape(x, (
+    #x = np.array([unwrap(first), unwrap(second)])
+    #x = np.reshape(x, (MAX_SECT_LEN, numLines))
+
+    #y = np.array([unwrap(second), unwrap(first)])
+    #y = np.reshape(y, (MAX_SECT_LEN, numLines))
+    x = [first, second]
+    y = [second,third]
+
+    return x,y
 
 
 def trainOn4(N_epochs):
 
+    numLines = 3
+    numPieces = 4
+    thsize = 20 # num ints in twoHotHorizontal
+
+    #NOT BEING TRAINED ON
     filename = 'ArtOfFugueExpoThreeLines.csv'
-    pred = Piece()
-    pred.fromCSV(filename)
-    predCC = pred.getCC()
+    x1,y1 = getPieceSections(filename, 16*4, 16*4, 16*4, numLines)
+    
 
     outfile = 'out.csv'
 
     filename = 'CBachWTC3Expo.csv'
-    p1 = Piece()
-    p1.fromCSV(filename)
-    p1cc = p1.getCC()
+    x2,y2 = getPieceSections(filename, 16*2+6, 16*2, 16*2, numLines)
 
     filename = 'CBachWTC9Expo.csv'
-    p2 = Piece()
-    p2.fromCSV(filename)
-    p2cc = p2.getCC()
+    x3,y3 = getPieceSections(filename, 6*4, 6*5, 6*4, numLines) # this one is in 3/8
 
     filename = 'CSeventhArtOfFugueExpo.csv'
-    p3 = Piece()
-    p3.fromCSV(filename)
-    p3cc = p3.getCC()
+    x4,y4 = getPieceSections(filename, 8*5, 8*5, 8*4+2, numLines) # this one is in 2/4
 
     filename = 'CbachFugue14Expo.csv'
-    p4 = Piece()
-    p4.fromCSV(filename)
-    p4cc = p4.getCC()
+    x5,y5 = getPieceSections(filename, 16*3, 16*3 + 6+14, 16*3+2, numLines)
+
+    #print "x1", x1
+
+    x = []
+    # concatenate all the arrays
+    for i in [x2,x3,x4,x5]:
+        x += unwrap(unwrap(unwrap(i)))
+        print "len single", len(unwrap(unwrap(unwrap(i))))
+
+
+    y = []
+    # concatenate all the arrays
+    for i in [y2,y3,y4,y5]:
+        y += unwrap(unwrap(unwrap(i)))
+        print "len single", len(unwrap(unwrap(unwrap(i))))
+
+    #x = unwrap(unwrap(x2))+unwrap(x3)+unwrap(x4)+unwrap(x5)
+    #print "len x2", len(unwrap(unwrap(x2)))
+    print "lenx", len(x)
+
+    #print "x", x
+    x_tot = np.array(x)
+    print "x_tot", x_tot
+    x_tot = np.reshape(x_tot, ( numPieces*(numLines-1),1,  CC_SIZE*numLines*MAX_SECT_LEN))
+    y_tot = np.array(y)
+    y_tot = np.reshape(y_tot, (numPieces*(numLines-1), CC_SIZE*numLines*MAX_SECT_LEN))
+    print "x_tot", x_tot
 
     #build_model(p1, len(p1), 8)
-    thsize = 20 # num ints in twoHotHorizontal
-    numLines = 3
-    numPieces = 4
-    m = modelCCPadding([p1cc], numPieces, numLines, thsize, N_epochs)
+    
+    m = modelRecurrent(x_tot,y_tot,numPieces,numLines, N_epochs)
+    
+    #m = modelCCPadding([p1cc], numPieces, numLines, thsize, N_epochs)
 
-    v = '4'
+    v = '10'
 
-    predictStuffPadding('outAOF' + v + '.csv', predCC, numLines, thsize, m)
-    predictStuffPadding('outWTC3' + v + '.csv', p1cc, numLines, thsize, m)
-    predictStuffPadding('outWTC9' + v + '.csv', p2cc, numLines, thsize, m)
-    predictStuffPadding('outSAOF' + v + '.csv', p3cc, numLines, thsize, m)
-    predictStuffPadding('outFF' + v + '.csv', p4cc, numLines, thsize, m)
+
+    predictStuff('outAOF' + v + '.csv', x1[0], x1[1], numLines, m)
+    predictStuff('outWTC3' + v + '.csv', x2[0], x2[1], numLines, m)
+    predictStuff('outWTC9' + v + '.csv', x3[0], x3[1], numLines, m)
+    predictStuff('outSAOF' + v + '.csv', x4[0], x4[1], numLines, m)
+    predictStuff('outFF' + v + '.csv', x5[0], x5[1], numLines, m)
+
+
+
+def predictStuff(outfile, first, second, numLines, model):
+    first = np.array([unwrap(unwrap(unwrap(first)))])
+    first = np.reshape(first, (1, 1,  CC_SIZE*numLines*MAX_SECT_LEN))
+    second = np.array([unwrap(unwrap(unwrap(second)))])
+    second = np.reshape(second, (1, 1,  CC_SIZE*numLines*MAX_SECT_LEN))
+
+    print "FIRST", first
+    pred = model.predict(first)
+    print "pred1", pred
+
+    # make it a standard shape
+    pred = np.reshape(pred, (MAX_SECT_LEN,numLines, CC_SIZE))
+    predList = np.asarray(pred)
+    CCpred = []
+    for time in predList:
+        t = []
+        for note in time:
+            t.append(goodRepCC(note))
+        CCpred.append(t)
+
+    print "pred2", CCpred
+
+    predPiece = fromCC(CCpred)
+    predPieceCsv = open(outfile, 'w')
+    predPieceCsv.write(predPiece.csv())
+
 
 
 
@@ -395,7 +493,7 @@ def predictStuffPadding(outfile, predCC, numLines, thsize, m):
 
 
 
-def predictStuff(outfile, predCC, numLines, thsize, m):
+def predictStuff1(outfile, predCC, numLines, thsize, m):
 
 
     first = predCC[0: 0 + TMP_MAX_LEN]
@@ -529,7 +627,7 @@ def trainFullFugue(N_epochs):
 def main(args):
     #a = [0.8633742928504944, 0.8520313501358032, 0.6155416965484619, 0.8277215957641602, 0.7745899558067322, 0.6654524803161621, 0.5017514824867249, 0.5754479169845581, 0.3196893334388733, 0.364773690700531, 0.2010064721107483, 0.3020211458206177, 0.21267035603523254, 0.22699597477912903, 0.23443511128425598, 0.2467953860759735, 0.32214605808258057, 0.1710146963596344, 0.18880566954612732, 0.21740081906318665]
     #print goodRepCC(a)
-    trainOn4(250)
+    trainOn4(300)
 
 
 
